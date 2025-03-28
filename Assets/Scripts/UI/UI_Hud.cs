f76cb92b14154d5459ce3b72fbd1778086147c43
KSangMin <fhfpszmfnwm@naver.com>
2025-03-28 12:19:25 +0900

    [Feat] 각 UI 기본 스크립트 구현

--- Assets/Scripts/UI/UI_Hud.cs
+++ Assets/Scripts/UI/UI_Hud.cs
@@ -1,6 +1,29 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Hud : UI
 {
-    
+    [SerializeField] private TextMeshProUGUI hpText;
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI killText;
+
+    [SerializeField] private Image hpBar;
+    [SerializeField] private Image expBar;
+
+    [SerializeField] private Transform skillSlotsParentTransform;
+    [SerializeField] private Transform statSlotsParentTransform;
+
+    private List<UI_SkillSlot> _skillSlots;
+    private List<UI_StatSlot> _statSlots;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _skillSlots = new List<UI_SkillSlot>(skillSlotsParentTransform.GetComponentsInChildren<UI_SkillSlot>());
+        _statSlots = new List<UI_StatSlot>(statSlotsParentTransform.GetComponentsInChildren<UI_StatSlot>());
+    }
 }
--- Assets/Scripts/UI/UI_StatSlot.cs
+++ Assets/Scripts/UI/UI_StatSlot.cs
@@ -1,7 +1,18 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [SelectionBase]
 public class UI_StatSlot : UI
 {
-    
+    [SerializeField] private Image icon;
+
+    [SerializeField] private TextMeshProUGUI levelText;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        icon.gameObject.SetActive(false);
+    }
 }
