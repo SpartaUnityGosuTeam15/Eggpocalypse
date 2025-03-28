50d3ef96bc4cbbb092fef0588de26209a309c542
KSangMin <fhfpszmfnwm@naver.com>
2025-03-28 17:48:52 +0900

    [Feat] 플레이어 모델링 및 애니메이션 추가

--- Assets/Scripts/Objects/Character/Player/PlayerController.cs
+++ Assets/Scripts/Objects/Character/Player/PlayerController.cs
@@ -5,15 +5,20 @@ public class PlayerController : MonoBehaviour
 {
     private InputHandler _input;
     private Rigidbody _rb;
+    private Animator _animator;
+    [SerializeField] private Transform model;
 
     [Range(1f, 10f)]
     public float speed = 5f;
     private Vector2 curMovement;
 
+    private bool _hasTarget = false;
+
     private void Start()
     {
         _input = GetComponent<InputHandler>();
         _rb = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
 
         RemoveInputActionCallbacks();
         AddInputActionCallbacks();
@@ -38,13 +43,43 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputAction.CallbackContext context)
     {
-        if(context.performed) curMovement = context.ReadValue<Vector2>();
-        else if(context.canceled) curMovement = Vector2.zero;
+        if (context.performed) curMovement = context.ReadValue<Vector2>();
+        else if (context.canceled) curMovement = Vector2.zero;
     }
 
     void Move()
     {
         Vector3 dir = Vector3.forward * curMovement.y + Vector3.right * curMovement.x;
         _rb.velocity = dir * speed;
+
+        if (_hasTarget) { } //Look(target);
+        else Look(dir);
+
+        if(dir != Vector3.zero)
+        {
+            _animator.SetBool("isMoving", true);
+            Vector3 localMove = model.InverseTransformDirection(dir);
+            _animator.SetFloat("dX", localMove.x);
+            _animator.SetFloat("dY", localMove.z);
+        }
+        else
+        {
+            _animator.SetBool("isMoving", false);
+        }
+    }
+
+    public void Look(Transform target)
+    {
+        Vector3 dir = (target.position - transform.position).normalized;
+        Look(dir);
+    }
+
+    public void Look(Vector3 dir)
+    {
+        if(dir == Vector3.zero) return;
+
+        Quaternion targetRotation = Quaternion.LookRotation(dir);
+        targetRotation.eulerAngles = targetRotation.eulerAngles + new Vector3(0, 30, 0);
+        model.rotation = Quaternion.Slerp(model.rotation, targetRotation, 0.2f);
     }
 }
