f2d6ca24a06382331219d6874ed51691d25e2fa9
Hwanyi <wksgo123@knu.ac.kr>
2025-04-01 18:01:10 +0900

    [Feat] 스킬 가져가는 함수 추가

--- Assets/Scripts/Managers/SkillManager.cs
+++ Assets/Scripts/Managers/SkillManager.cs
@@ -1,47 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class SkillManager : Singleton<SkillManager>
 {
-    public List<GameObject> skills;
     public GameObject[] attackSkillPrefabs;
+    Dictionary<int, SkillData> skillDict = DataManager.Instance.skillDict;
 
     private void Start()
     {
-        //JsonLoader를 이용해서 스킬 및 스탯 옵션 정보 받기
         DataManager.Instance.Init();
-        Dictionary<int, SkillData> skillDict = DataManager.Instance.skillDict;
-        foreach (var skillData in skillDict.Values)
+    }
+
+    public AttackSkill GetSkill(int id, Transform trans)
+    {
+        GameObject go;
+        SkillData skillData;
+        if(!skillDict.TryGetValue(id, out skillData))
         {
-            AttackSkill skill;
-            GameObject go;
-            
-            if (skillData.skillType == SkillType.ProjectileSkill)
-                go = Instantiate(attackSkillPrefabs[0]);
-               
-            else if (skillData.skillType == SkillType.CircleSkill)
-                go = Instantiate(attackSkillPrefabs[1]);
-                
-            else
-                go = Instantiate(attackSkillPrefabs[2]);
-
-            skill = go.GetComponent<AttackSkill>();
-            skill.skillData = skillData;
-
-            skill.skillName = skillData.name;
-            skill.skillDescription = skillData.description;
-            skill.skillLevel = 0;
-            skill.damage = skillData.damage;
-            skill.attackRate = skillData.attackRate;
-            skill.attackRange = skillData.attackRange;
-            skill.penetration = skillData.penetration;
-            skill.shotSpeed = skillData.shotSpeed;
-            skill.shotCount = skillData.shotCount;
-            skill.isAuto = skillData.autoAttack;
-            
-            skills.Add(go);
-            go.SetActive(false);
+            return null; //id 값이 없는 경우
         }
+
+        if (skillData.skillType == SkillType.ProjectileSkill)
+            go = Instantiate(attackSkillPrefabs[0], trans);
+
+        else if (skillData.skillType == SkillType.CircleSkill)
+            go = Instantiate(attackSkillPrefabs[1], trans);
+
+        else
+            go = Instantiate(attackSkillPrefabs[2], trans);
+
+        AttackSkill skill = go.GetComponent<AttackSkill>();
+        skill.skillData = skillData;
+
+        skill.skillName = skillData.name;
+        skill.skillDescription = skillData.description;
+        skill.skillLevel = 0;
+        skill.damage = skillData.damage;
+        skill.attackRate = skillData.attackRate;
+        skill.attackRange = skillData.attackRange;
+        skill.penetration = skillData.penetration;
+        skill.shotSpeed = skillData.shotSpeed;
+        skill.shotCount = skillData.shotCount;
+        skill.isAuto = skillData.autoAttack;
+
+        return skill;
     }
 }
