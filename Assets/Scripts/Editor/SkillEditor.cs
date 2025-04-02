9391023a5081ef65c5b878eb56c6d2162ebf5ced
Hwanyi <wksgo123@knu.ac.kr>
2025-04-02 10:50:23 +0900

    [Feat] SkillManager 추가

--- Assets/Scripts/Editor/SkillEditor.cs
+++ Assets/Scripts/Editor/SkillEditor.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(BaseSkill), true)]
+[CustomEditor(typeof(AttackSkill), true)]
 public class SkillEditor : Editor
 {
     BaseSkill skill;
